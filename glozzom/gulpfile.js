36f1b7a31e27247da4d9b1e1ae25838d33d77b52
aamirmukaram <aamirmukaram@gmail.com>
2020-09-11 19:26:47 +0200

    Completed glozzom theme, services page

--- glozzom/gulpfile.js
+++ glozzom/gulpfile.js
@@ -64,7 +64,8 @@ function copyVendors() {
     return gulp.src([
         'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
         'node_modules/ekko-lightbox/dist/ekko-lightbox.js',
-        'node_modules/jquery/dist/jquery.min.js'
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/slick-carousel/slick/slick.js'
     ]).pipe(gulp.dest('docs/js/'));
 }
 
---
+++ glozzom/src/js/about.js
@@ -0,0 +1,8 @@
+(function () {
+    $(document).ready(function () {
+        $('#slick-slider').slick({
+            nextArrow: '<i class="fas fa-arrow-circle-right slick-next text-white"></i>',
+            prevArrow: '<i class="fas fa-arrow-circle-left slick-prev text-white"></i>',
+        });
+    });
+})();
